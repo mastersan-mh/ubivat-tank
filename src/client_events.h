70990be407fcfcdcd1eed7e14b1c1184a38bd005
mastersan <mastersan@be6be5c5-49bb-43f0-ac2a-720554014b3b>
2017-11-06 11:32:12 +0000

    * state machines fixed.

--- src/client_events.h
+++ src/client_events.h
@@ -57,5 +57,6 @@ typedef struct
 } game_client_event_t;
 
 extern void client_event_local_key_input(bool key_repeat, int key, bool state);
+extern void client_event_local_entergame();
 
 #endif /* SRC_CLIENT_EVENTS_H_ */
